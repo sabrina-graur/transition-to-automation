429cb205000a36370de81a6c6553206f6d872ba8
sabrina-graur <sabrina.graur@gmail.com>
2023-11-27 10:41:05 +0200

    exercises-27 nov 2023

---
+++ src/main/java/mainclass/exercises/Planet.java
@@ -0,0 +1,14 @@
+package mainclass.exercises;
+
+public enum Planet {
+    MERCURY(1.1), VENUS(2.2), EARTH(7.7), MARS(8.88), JUPITER(11.0), SATURN(12), URANUS(18), NEPTUNE(0.2);
+    double gravity;
+
+    Planet(double gravity) {
+        this.gravity = gravity;
+    }
+
+    double getGravity() {
+        return gravity;
+    }
+}
